94c52fc17759a634d70cb8366cbebf267831644c
Savindu <blsthathsara@gmail.com>
2021-09-22 01:47:31 +0530

    UI Design in Welcome & signIn activity Signed-off-by: Savindu <blsthathsara@gmail.com>

--- app/src/main/java/com/example/demo1/MainActivity.java
+++ app/src/main/java/com/example/demo1/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.demo1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnWelcome = (Button) findViewById(R.id.btn_welcome_start);
+        btnWelcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSignIn();
+            }
+        });
+    }
+
+    public void openSignIn(){
+        Intent intent = new Intent(this, SignIn.class);
+        startActivity(intent);
     }
 }
